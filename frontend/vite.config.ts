600026955ce8cb21eb793cc59373bb5731680525
Felipe Cordeiro Ferreira <felicordeirofcf@gmail.com>
2025-05-06 13:16:25 -0300

    Sua mensagem de commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,11 +13,22 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: process.env.VITE_API_URL || "http://localhost:3001", // Use variable for API URL or default to localhost
         changeOrigin: true,
-        // secure: false,
+        secure: process.env.VITE_API_URL ? true : false, // Set secure to true in production environments
       },
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Group all dependencies into a single chunk
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit (default is 500 KB)
+  },
 });
-
