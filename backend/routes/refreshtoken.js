75b7352d6bb3ce5a97e17d004458708c924d02c1
Felipe Cordeiro Ferreira <felicordeirofcf@gmail.com>
2025-05-19 12:03:24 -0300

    Removendo a pasta temp do controle de versão

---
+++ backend/routes/refreshtoken.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const refreshTokenSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  token: {
+    type: String,
+    required: true
+  },
+  expiresAt: {
+    type: Date,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: '30d' // Documento será automaticamente removido após 30 dias
+  }
+});
+
+const RefreshToken = mongoose.model('RefreshToken', refreshTokenSchema);
+
+module.exports = RefreshToken;
